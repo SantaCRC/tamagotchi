e905ecad37f69ee049bbfecbec52e5f17d0d8e59
Fabian Alvarez <35088759+SantaCRC@users.noreply.github.com>
2023-08-24 16:49:40 -0600

    blocking

--- src/stats.v
+++ src/stats.v
@@ -1,4 +1,4 @@
-// module to manage tamagotchi's stats for the game, exaple: hunger, happiness, etc.
+// module to manage tamagotchi's stats for the game, example: hunger, happiness, etc.
 module stats(
     input wire clk, // 27 MHz clock
     output reg [3:0] hunger,
@@ -20,32 +20,32 @@ always @(posedge clk) begin
         case (random)
             0: begin
                 if (hunger < 4'd15) begin
-                    hunger <= hunger + 1;
+                    hunger = hunger + 1;
                 end
             end
             1: begin
                 if (happiness < 4'd15) begin
-                    happiness <= happiness + 1;
+                    happiness = happiness + 1;
                 end
             end
             2: begin
                 if (health < 4'd15) begin
-                    health <= health + 1;
+                    health = health + 1;
                 end
             end
             3: begin
                 if (hygiene < 4'd15) begin
-                    hygiene <= hygiene + 1;
+                    hygiene = hygiene + 1;
                 end
             end
             4: begin
                 if (energy < 4'd15) begin
-                    energy <= energy + 1;
+                    energy = energy + 1;
                 end
             end
             5: begin
                 if (social < 4'd15) begin
-                    social <= social + 1;
+                    social = social + 1;
                 end
             end
         endcase
@@ -59,37 +59,34 @@ end
 always @(posedge clk) begin
     if (inputs[0] == 1'b1) begin
         if (hunger > 4'd0) begin
-            hunger <= hunger - 1;
+            hunger = hunger - 1;
         end
     end
     if (inputs[1] == 1'b1) begin
         if (happiness > 4'd0) begin
-            happiness <= happiness - 1;
+            happiness = happiness - 1;
         end
     end
     if (inputs[2] == 1'b1) begin
         if (health > 4'd0) begin
-            health <= health - 1;
+            health = health - 1;
         end
     end
     if (inputs[3] == 1'b1) begin
         if (hygiene > 4'd0) begin
-            hygiene <= hygiene - 1;
+            hygiene = hygiene - 1;
         end
     end
     if (inputs[4] == 1'b1) begin
         if (energy > 4'd0) begin
-            energy <= energy - 1;
+            energy = energy - 1;
         end
     end
     if (inputs[5] == 1'b1) begin
         if (social > 4'd0) begin
-            social <= social - 1;
+            social = social - 1;
         end
     end
 end
-
-
-
 
 endmodule
