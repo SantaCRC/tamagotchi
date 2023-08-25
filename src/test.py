3e018a612f1e6fa9c94513ad89449c316f374845
Fabian Alvarez <35088759+SantaCRC@users.noreply.github.com>
2023-08-24 19:41:15 -0600

    time in seconds

--- src/test.py
+++ src/test.py
@@ -1,6 +1,6 @@
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
+from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles, Timer
 
 
 
@@ -13,15 +13,14 @@ async def status(dut):
 
     # reset
     dut._log.info("reset")
-    dut.rst_n.value = 0
+    dut.rst_n.value = 1
     # set the compare value
     dut.ui_in.value = 1
     await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
 
     dut._log.info("check status") 
     for i in range(10):
-        await ClockCycles(dut.clk, 1)
+        await Timer(1, units='sec')
         dut._log.info("status: %d" % dut.status.value)
     
 
