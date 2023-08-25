474ee3ec2cd79387afb9b8c3d3c1142d00e604bd
Fabian Alvarez <35088759+SantaCRC@users.noreply.github.com>
2023-08-24 20:47:42 -0600

    inverse reset

--- src/test.py
+++ src/test.py
@@ -13,9 +13,9 @@ async def status(dut):
 
     # reset
     dut._log.info("reset")
-    dut.rst_n.value = 1
-    await ClockCycles(dut.clk, 1)
     dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 1)
+    dut.rst_n.value = 1
     # set the compare value
     dut.ui_in.value = 1
     await ClockCycles(dut.clk, 10)
