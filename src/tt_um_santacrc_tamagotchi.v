94990748890ea775bd8f59ed249bd1261b9b4fca
Fabian Alvarez <35088759+SantaCRC@users.noreply.github.com>
2023-08-27 16:54:44 -0600

    iu

--- src/tt_um_santacrc_tamagotchi.v
+++ src/tt_um_santacrc_tamagotchi.v
@@ -28,15 +28,15 @@ module tt_um_santacrc_tamagotchi #( parameter MAX_COUNT = 24'd10_000_000 ) (
 
     // status register
     reg [6:0] status;
-
-    // output status to leds
-    assign uio_out = status;
     
     // random number
     wire [7:0] random_number;
 
     // use bidirectionals as outputs
     assign uio_oe = 8'b11111111;
+
+    // output status to leds
+    assign uio_out = status;
 
 
     // call stats module
