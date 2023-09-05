cbfc3bb7523fd06b233321d2c25a625a796aa895
Fabian Alvarez <35088759+SantaCRC@users.noreply.github.com>
2023-09-04 22:34:25 -0600

    t

--- src/random.v
+++ src/random.v
@@ -9,7 +9,7 @@ reg [7:0] lfsr_reg;          // 8-bit register to hold the LFSR state
 
 always @(posedge clk or posedge rst) begin
     if (rst) begin
-        lfsr_reg <= dataIn[7:0]; // Initialize LFSR with all ones
+        lfsr_reg = dataIn[7:0]; // Initialize LFSR with all ones
     end else begin
         // XOR feedback taps for an 8-bit LFSR: 8, 6, 5, 4
         lfsr_reg <= {lfsr_reg[6:0], lfsr_reg[7] ^ lfsr_reg[4] ^ lfsr_reg[5] ^ lfsr_reg[3]};
